0bcca97b3a0eee1d967a5eeec1b6c3c3c75c5b8e
Tony <99278605+tonycurra@users.noreply.github.com>
2025-01-11 15:04:32 +0100

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -5,6 +5,8 @@ document.querySelector('#connect').addEventListener('click', function () {
     statusElem.textContent = 'Scanning for devices...';
     debugElem.textContent = '';
 
+    let characteristic;
+
     navigator.bluetooth.requestDevice({
         filters: [{ services: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b'] }]
     })
@@ -21,7 +23,8 @@ document.querySelector('#connect').addEventListener('click', function () {
     })
     .then(server => server.getPrimaryService('4fafc201-1fb5-459e-8fcc-c5c9c331914b'))
     .then(service => service.getCharacteristic('beb5483e-36e1-4688-b7f5-ea07361b26a8'))
-    .then(characteristic => {
+    .then(char => {
+        characteristic = char;
         debugElem.textContent += 'Characteristic found!\n';
         statusElem.textContent = 'Connected! Ready to control LED';
 
@@ -31,6 +34,31 @@ document.querySelector('#connect').addEventListener('click', function () {
                 const value = new TextDecoder().decode(event.target.value);
                 debugElem.textContent += `Notification received: ${value}\n`;
             });
+        });
+    })
+    .then(() => {
+        // Bind ON button
+        document.querySelector('#ledOn').addEventListener('click', () => {
+            if (characteristic) {
+                const command = new TextEncoder().encode('ON');
+                characteristic.writeValue(command).then(() => {
+                    debugElem.textContent += 'Sent: ON\n';
+                }).catch(err => {
+                    debugElem.textContent += `Error sending ON: ${err.message}\n`;
+                });
+            }
+        });
+
+        // Bind OFF button
+        document.querySelector('#ledOff').addEventListener('click', () => {
+            if (characteristic) {
+                const command = new TextEncoder().encode('OFF');
+                characteristic.writeValue(command).then(() => {
+                    debugElem.textContent += 'Sent: OFF\n';
+                }).catch(err => {
+                    debugElem.textContent += `Error sending OFF: ${err.message}\n`;
+                });
+            }
         });
     })
     .catch(error => {
