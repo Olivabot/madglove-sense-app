1d015c57edc581650002c5764716cfbca302ac36
Tony <99278605+tonycurra@users.noreply.github.com>
2025-01-11 14:37:09 +0100

    Update script.js for xiao esp32s3

--- app/js/script.js
+++ app/js/script.js
@@ -3,7 +3,13 @@ document.querySelector('#connect').addEventListener('click', function () {
         .requestDevice({
             filters: [{ services: ['19b10000-e8f2-537e-4f6c-d104768a1214'] }]
         })
-        .then(device => device.gatt.connect())
+        .then(device => {
+            device.addEventListener('gattserverdisconnected', () => {
+                document.querySelector('#status').textContent = 'Disconnected';
+                document.querySelector('#sensorData').textContent = '';
+            });
+            return device.gatt.connect();
+        })
         .then(server => server.getPrimaryService('19b10000-e8f2-537e-4f6c-d104768a1214'))
         .then(service => service.getCharacteristic('19b10002-e8f2-537e-4f6c-d104768a1214'))
         .then(characteristic => {
